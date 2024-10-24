c0b38ef74ddf55373aa363a4267ad463f78e65f8
pasindu100 <dilshanp5400@gmail.com>
2024-10-24 12:09:46 +0530

    create required  pages and routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Pages/Home";
+import About from "./Components/Pages/About";
+import Signin from "./Components/Pages/Signin";
+import Signup from "./Components/Pages/Signup";
+import Dashboard from "./Components/Pages/Dashboard";
+import Project from "./Components/Pages/Project";
 
 function App() {
   return (
-    <div className='bg-slate-500'>App</div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Project />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
