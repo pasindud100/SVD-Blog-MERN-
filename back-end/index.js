733b83492424e4f1c20e28f2dd048f05dc42ce64
pasindud100 <dilshanp5400@gmail.com>
2024-11-01 01:59:49 -0700

    connect mongo db atlas

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,18 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+mongoose
+  .connect(
+    "mongodb+srv://dilshanp5400:DbPasindu@cluster0.xbuulqh.mongodb.net/mern-blog?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB", error);
+  });
+
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
 });
