e318d7d0f8dfede0e26dbe73f984cfd79694c23a
pasindud100 <dilshanp5400@gmail.com>
2024-11-06 04:14:07 -0800

    combine redux persist to login

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -2,8 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { store } from "./redux/store.js";
+import { store, persistor } from "./redux/store.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 // Get the root DOM element
 const rootElement = document.getElementById("root");
@@ -11,9 +12,11 @@ const rootElement = document.getElementById("root");
 // Create a React root and render the app
 const root = createRoot(rootElement);
 root.render(
-  <StrictMode>
-    <Provider store={store}>
+<PersistGate persistor={persistor}>
+<Provider store={store}>
       <App />
     </Provider>
-  </StrictMode>
+</PersistGate>
+   
+
 );
